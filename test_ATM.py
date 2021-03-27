104ffc1b9819450967ce9179e64e895cc627ca54
Chae Rim Kim <kcl0720@gmail.com>
2021-03-27 23:35:21 +0900

    Reformat code and fix comments

--- test_ATM.py
+++ test_ATM.py
@@ -5,31 +5,31 @@ from ATM import Account, ATMController
 class TestAccount(unittest.TestCase):
     def setUp(self):
         self.test_account = Account()
-        self.test_account.create_user(11112222, 1234, "credit", 100)
-        self.test_account.create_user(11112222, 1234, "savings", 20000)
-        self.test_account.create_user(33334444, 9999, "savings", 1400)
+        self.test_account.create_account(11112222, 1234, "credit", 100)
+        self.test_account.create_account(11112222, 1234, "savings", 20000)
+        self.test_account.create_account(33334444, 9999, "savings", 1400)
 
-    def test_verify_user(self):
+    def test_verify_account(self):
         # Test with correct pin and account
-        valid_user = self.test_account.verify_user(11112222, 1234)
+        valid_user = self.test_account.verify_account(11112222, 1234)
         self.assertEqual(valid_user,
                          [{'account_type': 'credit', 'balance': 100}, {'account_type': 'savings', 'balance': 20000}])
 
         # Test wrong pin
-        invalid_user = self.test_account.verify_user(33334444, 0000)
+        invalid_user = self.test_account.verify_account(33334444, 0000)
         self.assertFalse(invalid_user)
 
         # Test account that doesnt exist
-        nonexistent_user = self.test_account.verify_user(88889999, 0000)
+        nonexistent_user = self.test_account.verify_account(88889999, 0000)
         self.assertFalse(nonexistent_user)
 
 
 class TestATMController(unittest.TestCase):
     def setUp(self):
         self.test_account = Account()
-        self.test_account.create_user(11112222, 1234, "credit", 100)
-        self.test_account.create_user(11112222, 1234, "savings", 20000)
-        self.test_account.create_user(33334444, 9999, "savings", 1400)
+        self.test_account.create_account(11112222, 1234, "credit", 100)
+        self.test_account.create_account(11112222, 1234, "savings", 20000)
+        self.test_account.create_account(33334444, 9999, "savings", 1400)
 
         self.test_ATM = ATMController(self.test_account)
 
@@ -46,7 +46,8 @@ class TestATMController(unittest.TestCase):
     def test_select_account(self):
         selected_account = self.test_ATM.select_account(
             [{'account_type': 'credit', 'balance': 100}, {'account_type': 'savings', 'balance': 20000}], "credit")
-        self.assertEqual(selected_account, {'account_type': 'credit', 'balance': 100})
+        self.assertEqual(selected_account, {
+                         'account_type': 'credit', 'balance': 100})
 
         # Test account that doesnt exist
         nonexistent_account = self.test_ATM.select_account(
@@ -55,28 +56,37 @@ class TestATMController(unittest.TestCase):
 
     def test_perform_tasks(self):
         # prints correct balance
-        correct_balance = self.test_ATM.perform_tasks({'account_type': 'credit', 'balance': 100}, "See Balance")
+        correct_balance = self.test_ATM.perform_tasks(
+            {'account_type': 'credit', 'balance': 100}, "See Balance")
         self.assertEqual(correct_balance, 100)
 
         # Test valid deposit
-        valid_deposit = self.test_ATM.perform_tasks({'account_type': 'credit', 'balance': 100}, "Deposit", 100)
+        valid_deposit = self.test_ATM.perform_tasks(
+            {'account_type': 'credit', 'balance': 100}, "Deposit", 100)
         self.assertEqual(valid_deposit, 200)
 
         # Test negative deposit
-        invalid_deposit = self.test_ATM.perform_tasks({'account_type': 'credit', 'balance': 100}, "Deposit", -1000)
-        self.assertEqual(invalid_deposit, "Deposit amount must be bigger than zero")
+        invalid_deposit = self.test_ATM.perform_tasks(
+            {'account_type': 'credit', 'balance': 100}, "Deposit", -1000)
+        self.assertEqual(
+            invalid_deposit, "Deposit amount must be bigger than zero")
 
         # Test valid withdraw
-        valid_withdraw = self.test_ATM.perform_tasks({'account_type': 'credit', 'balance': 100}, "Withdraw", 40)
+        valid_withdraw = self.test_ATM.perform_tasks(
+            {'account_type': 'credit', 'balance': 100}, "Withdraw", 40)
         self.assertEqual(valid_withdraw, 60)
 
         # Test excessive withdraw - banned
-        invalid_withdraw = self.test_ATM.perform_tasks({'account_type': 'credit', 'balance': 100}, "Withdraw", 20000)
-        self.assertEqual(invalid_withdraw, "Withdraw amount exceeds the balance in your account")
+        invalid_withdraw = self.test_ATM.perform_tasks(
+            {'account_type': 'credit', 'balance': 100}, "Withdraw", 20000)
+        self.assertEqual(invalid_withdraw,
+                         "Withdraw amount exceeds the balance in your account")
 
         # Test withdraw amount = 0
-        zero_withdraw = self.test_ATM.perform_tasks({'account_type': 'credit', 'balance': 100}, "Withdraw", 0)
-        self.assertEqual(zero_withdraw, "Withdraw amount must be bigger than zero")
+        zero_withdraw = self.test_ATM.perform_tasks(
+            {'account_type': 'credit', 'balance': 100}, "Withdraw", 0)
+        self.assertEqual(
+            zero_withdraw, "Withdraw amount must be bigger than zero")
 
 
 if __name__ == '__main__':
